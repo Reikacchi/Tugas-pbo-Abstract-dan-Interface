75c8c083aaf58a10c28cd8bac67f20fa3b602dc4
Rifki Budiyanto <167438084+Reikacchi@users.noreply.github.com>
2024-10-03 14:35:12 +0700

    Add files via upload

---
+++ abstrack dan interface/Spider.java
@@ -0,0 +1,10 @@
+public class Spider extends Animal {
+    public Spider() {
+        super(8); // Spiders have 8 legs
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Sedang makan serangga");
+    }
+}
